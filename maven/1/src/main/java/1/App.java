77748d0d7311ef6c88dbde2777db46279f76a6a8
Ruan Franklin <94229859+Ruan-Franklin@users.noreply.github.com>
2023-05-24 20:32:54 +0000

    :chore: ci: configuração inicial do maven

---
+++ maven/1/src/main/java/1/App.java
@@ -0,0 +1,12 @@
+package br.edu.ifrn.projetomaven;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
